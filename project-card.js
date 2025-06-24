49a80dd7983b4debb61a930653fd2a6ddcf9daed
Rune Seeland <runeseeland@gmail.com>
2025-06-24 17:29:49 +0200

    refactor: clean up index and descriptions

--- project-card.js
+++ project-card.js
@@ -9,26 +9,25 @@ export class ProjectCard extends HTMLElement {
     const left = this.getAttribute("left") || "";
 
     this.innerHTML = `
-      <div id="${wrapperId}" class="projectElement ${
+      <a class="projectLinkWrapper" href="${url}">
+        <div id="${wrapperId}" class="projectElement ${
       left === "true" ? "blockViewLeft" : "blockViewCenter"
     } boxShadow hidden">
-        <a href="${url}">
-          <img alt="Thumbnail image of project" src="${image}" />
-        </a>
-        <div class="description">
-          <div class="projectHeaderContainer">
-            <h3>${title}</h3>
-            <img
-              src="${icon}"
-              class="skillIcons"
-            />
+            <img alt="Thumbnail image of project" src="${image}" />
+          <div class="description">
+            <div class="projectHeaderContainer">
+              <h3>${title}</h3>
+              <img
+                src="${icon}"
+                class="skillIcons"
+              />
+            </div>
+            <p>
+              ${description}
+            </p>
           </div>
-          <p>
-            ${description} ...
-            <a href="${url}">read more</a>
-          </p>
         </div>
-      </div>
+      </a>
     `;
   }
 }
