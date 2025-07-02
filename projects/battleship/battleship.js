ff0ac5d5589b5cecead0ad17524e316506181f0b
Rune Seeland <runeseeland@gmail.com>
2025-07-02 14:52:08 +0200

    battleship: add battleship class

---
+++ projects/battleship/battleship.js
@@ -0,0 +1,43 @@
+export class Battleship {
+  constructor(length) {
+    this.length = length;
+    this.positions = [];
+    this.shipArray = [];
+    this.direction = "";
+    for (let i = 0; i < length; i++) {
+      this.shipArray[i] = "o";
+    }
+  }
+
+  isWithinLength(length) {
+    return length >= 2 && length <= 5;
+  }
+
+  getLength() {
+    return this.length;
+  }
+
+  getShipArray() {
+    return this.shipArray;
+  }
+
+  getStartPos() {
+    return this.positions[0];
+  }
+
+  getEndPos() {
+    return this.positions[this.positions.length - 1];
+  }
+
+  getPositions() {
+    return this.positions;
+  }
+
+  getDirection() {
+    return this.direction;
+  }
+
+  setDirection(direction) {
+    this.direction = direction;
+  }
+}
